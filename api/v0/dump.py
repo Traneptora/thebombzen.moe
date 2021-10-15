78f0fd5e4d386f433d085e9becd9588256883c6c
Leo Izen <leo.izen@gmail.com>
2021-10-15 18:45:37 -0400

    dump: prelliminary dump

--- api/v0/dump.py
+++ api/v0/dump.py
@@ -1,8 +1,22 @@
 # python3 uwsgi
 # for /dump/
 
+import tempfile
+
+import requests
+
 from api_common import get_post_form
+
+with open('dump_webhook_url') as f:
+    webhook = f.read()
 
 def post(env):
     form = get_post_form(env)
-    return ('200 OK', {'success': 'successful test'})
+    if form['action'] == 'upload':
+        upload = form['upload']
+        if upload is not None and upload.filename is not None and upload.file is not None:
+            r = requets.post(webhook, files={'file': (upload.filename, upload.file)})
+            return (r.status_code, {'success': r.ok, 'response': r.json()})
+        else:
+            return ('400 Bad Request', 'Invalid Upload')
+    return ('400 Bad Request', 'Unsupported action')
