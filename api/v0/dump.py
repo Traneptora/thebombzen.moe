5f7895bf1c61a2f89cbe704a979c51b4cc864a26
Leo Izen <leo.izen@gmail.com>
2021-10-15 18:54:48 -0400

    easy acccess to url

--- api/v0/dump.py
+++ api/v0/dump.py
@@ -16,7 +16,14 @@ def post(env):
         upload = form['upload']
         if upload is not None and upload.filename is not None and upload.file is not None:
             r = requests.post(webhook, files={'file': (upload.filename, upload.file)})
-            return (str(r.status_code), {'success': r.ok, 'response': r.json()})
+            js = r.json()
+            ret = {'success': r.ok, 'response': js}
+            if 'attachments' in js and
+                    type(js['attachments']) is list and
+                    len(js['attachments']) > 0 and
+                    'url' in js['attachments'][0]:
+                ret['url'] = js['attachments'][0]['url']
+            return (str(r.status_code), ret)
         else:
             return ('400 Bad Request', 'Invalid Upload')
     return ('400 Bad Request', 'Unsupported action')
