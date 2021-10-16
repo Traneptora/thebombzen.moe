20ad73bb479854b5cfc64b31e96dd03f4a8e776e
Leo Izen <leo.izen@gmail.com>
2021-10-16 14:12:46 -0400

    found the bug

--- api/v0/dump.py
+++ api/v0/dump.py
@@ -5,7 +5,6 @@ import base64
 import binascii
 import re
 import zlib
-import traceback
 
 import requests
 
@@ -49,12 +48,11 @@ def get(env, relative_uri):
     except binascii.Error:
         return ('404 Not Found', 'Not base64url')
     try:
-        payload = zlib.decompress(comp_uri).decode()
-        s0 = int.from_bytes(payload[0:8], endian='little')
-        s1 = int.from_bytes(payload[8:16], endian='little')
+        payload = zlib.decompress(comp_uri)
+        s0 = int.from_bytes(payload[0:8], byteorder='little')
+        s1 = int.from_bytes(payload[8:16], byteorder='little')
         fname = payload[16:].decode()
         uri = f'https://cdn.discordapp.com/attachments/{s0}/{s1}/{fname}'
     except Exception:
-        traceback.print_exc()
         return ('404 Not Found', 'Bad Format')
     return ('302 Found', uri)
