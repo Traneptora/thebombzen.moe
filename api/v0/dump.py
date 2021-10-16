77c1b909be9ad6021d39914124763b1ee7ca9f1b
Leo Izen <leo.izen@gmail.com>
2021-10-16 14:27:35 -0400

    dont compress for now

--- api/v0/dump.py
+++ api/v0/dump.py
@@ -30,13 +30,14 @@ def post(env, relative_uri):
                 url = js['attachments'][0]['url']
             else:
                 return ('503 Service Unavailable', 'Backend Down')
-            match = re.match(r'^https://cdn\.discordapp\.com/attachments/([0-9]+)/([0-9]+)/(.*)$', url)
-            if match:
-                s0, s1, fname = match.group(1, 2, 3)
-                payload = int(s0).to_bytes(8, byteorder='little') + int(s1).to_bytes(8, byteorder='little') + fname.encode()
-                ret['url'] = 'https://thebombzen.moe/api/v0/dump/' + base64.b64encode(zlib.compress(payload, level=9), altchars=b'-_').decode()
-            else:
-                ret['url'] = url
+            # match = re.match(r'^https://cdn\.discordapp\.com/attachments/([0-9]+)/([0-9]+)/(.*)$', url)
+            # if match:
+            #     s0, s1, fname = match.group(1, 2, 3)
+            #     payload = int(s0).to_bytes(8, byteorder='little') + int(s1).to_bytes(8, byteorder='little') + fname.encode()
+            #     ret['url'] = 'https://thebombzen.moe/api/v0/dump/' + base64.b64encode(zlib.compress(payload, level=9), altchars=b'-_').decode()
+            # else:
+            #     ret['url'] = url
+            ret['url'] = url
             return (str(r.status_code), ret, [('access-control-allow-origin', '*')])
         else:
             return ('400 Bad Request', 'Invalid Upload')
