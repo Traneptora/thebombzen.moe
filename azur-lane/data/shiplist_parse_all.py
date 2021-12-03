fd422f23cc451034043faa7bb2efe3977cb30010
Leo Izen <leo.izen@gmail.com>
2021-12-02 22:04:58 -0500

    shiplist_parse_all: add newlines at end of json files

--- azur-lane/data/shiplist_parse_all.py
+++ azur-lane/data/shiplist_parse_all.py
@@ -7,11 +7,15 @@ from shiplist_parse_carrier import parse_carrier_json
 
 cargolist = []
 
+def dump_json(obj, jsonfile):
+    json.dump(obj, jsonfile, sort_keys=True, indent=4)
+    jsonfile.write('\n')
+
 for shiplist_json in ["shiplist_0.json", "shiplist_1.json"]:
     with open(shiplist_json, "r", encoding="UTF-8") as jsonfile:
         jsonlist = json.load(jsonfile)
     with open(shiplist_json, "w", encoding="UTF-8") as jsonfile:
-        json.dump(jsonlist, jsonfile, sort_keys=True, indent=4)
+        dump_json(jsonlist, jsonfile)
     cargolist += jsonlist["cargoquery"]
 
 toc = {"battleships":"battleships.json", "carriers":"carriers.json", "ships":[]}
@@ -30,25 +34,25 @@ for entry in cargolist:
     except FileExistsError:
         pass
     with open(shipdir + "/ship.json", "w", encoding="UTF-8") as jsonfile:
-        json.dump(ship, jsonfile, sort_keys=True, indent=4)
+        dump_json(ship, jsonfile)
     toc_entry = {"ShipID":shipID, "Name":shipName, "DataDir":shipdir, "dataJSON":(shipdir + "/ship.json")}
     if ship["Type"] in ["Battleship", "Battlecruiser", "Aviation Battleship", "Monitor"]:
         toc_entry["battleshipJSON"] = shipdir + "/battleship.json"
         battleships["ships"].append(toc_entry)
         battleship_json = parse_battleship_json(ship_json=ship)
         with open(toc_entry["battleshipJSON"], "w", encoding="UTF-8") as jsonfile:
-            json.dump(battleship_json, jsonfile, sort_keys=True, indent=4)
+            dump_json(battleship_json, jsonfile)
     if ship["Type"] in ["Aircraft Carrier", "Light Aircraft Carrier", "Aviation Battleship", "Submarine Carrier"] or "SubtypeRetro" in ship and ship["SubtypeRetro"] in ["Aviation Battleship"]:
         toc_entry["carrierJSON"] = shipdir + "/carrier.json"
         carriers["ships"].append(toc_entry)
         carrier_json = parse_carrier_json(ship_json=ship)
         with open(toc_entry["carrierJSON"], "w", encoding="UTF-8") as jsonfile:
-            json.dump(carrier_json, jsonfile, sort_keys=True, indent=4)
+            dump_json(carrier_json, jsonfile)
     toc["ships"].append(toc_entry)
 
 with open("ships/toc.json", "w", encoding="UTF-8") as jsonfile:
-    json.dump(toc, jsonfile, sort_keys=True, indent=4)
+    dump_json(toc, jsonfile)
 with open("ships/battleships.json", "w", encoding="UTF-8") as jsonfile:
-    json.dump(battleships, jsonfile, sort_keys=True, indent=4)
+    dump_json(battleships, jsonfile)
 with open("ships/carriers.json", "w", encoding="UTF-8") as jsonfile:
-    json.dump(carriers, jsonfile, sort_keys=True, indent=4)
+    dump_json(carriers, jsonfile)
