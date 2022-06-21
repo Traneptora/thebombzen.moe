439e0f639ae7e37fd01c8c8b361d19d13f7d6971
Leo Izen <leo.izen@gmail.com>
2022-06-21 12:15:48 -0400

    fix str comparison

--- azur-lane/data/shiplist_parse_battleship.py
+++ azur-lane/data/shiplist_parse_battleship.py
@@ -4,8 +4,8 @@ def parse_battleship_json(ship_json):
     battleship_json = {}
     battleship_json['Name'] = ship_json['Name']
     reload_pre_kai = int(ship_json['Reload125'])
-    reload_kai_125 = ship_json.get('ReloadKai125', 0)
-    reload_kai_125 = reload_kai_125 if reload_kai_125 is not None else 0
+    reload_kai_125 = ship_json['ReloadKai125']
+    reload_kai_125 = int(reload_kai_125) if reload_kai_125 is not None else 0
     reload_stat = reload_kai_125 if reload_kai_125 > 0 else reload_pre_kai
     battleship_json['Reload'] = reload_stat
     battleship_json['ReloadUnkai'] = reload_pre_kai
